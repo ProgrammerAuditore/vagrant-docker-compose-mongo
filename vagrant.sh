9cbaca5fad8d0d9d376369af1d4348919182e116
ProgrammerAuditore <victorvj098@gmail.com>
2023-04-06 18:19:32 -0500

    ***** dev-service-mongoose : Se modificio script de vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -1,12 +1,12 @@
 # Este es un script para Vagranfile
 # vagrant provision
 echo "[...] Provision iniciando [...]"
-sudo rm -rf ~/data
-sudo mkdir ~/data
+sudo rm -rf /home/vagran/data
+sudo mkdir -p /home/vagran/data
 cd /home/vagrant/workspace
 ls -lsa
 docker-compose stop
 docker-compose rm --force
-docker-compose build
+docker-compose build --no-cache
 docker-compose up -d
 echo "[...] Provision finalizado exitosamente ! [...]"
